4290561cbf6e9ede7eaf904b9220aa4fc62642ea
Tahira Baig <tahirabaig786@hotmail.com>
2019-11-16 11:44:12 +0500

    Login Registration and ForgetPasword Activity created

---
+++ app/src/main/java/lcwu/fyp/smartbin/ForgetPaswordActivity.java
@@ -0,0 +1,14 @@
+package lcwu.fyp.smartbin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgetPaswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_pasword);
+    }
+}
--- app/src/main/java/lcwu/fyp/smartbin/SplashActivity.java
+++ app/src/main/java/lcwu/fyp/smartbin/SplashActivity.java
@@ -1,8 +1,12 @@
 package lcwu.fyp.smartbin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.arch.core.util.Function;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.rbddevs.splashy.Splashy;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +14,45 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        setSplashy();
+
+    }
+
+    void setSplashy() {
+
+        new Splashy(this)
+                .setLogo(R.drawable.logo)
+                .setTitle("Smart Bin")
+                .setTitleColor("#FFFFFF")
+                .setSubTitle("Smart bin is usfull for envorment")
+                .setProgressColor(R.color.colorAccent)
+                .setBackgroundResource(R.drawable.ic_launcher_background)
+//                .setBackgroundResource("#000000")
+                .setFullScreen(true)
+                .setTime(5000)
+                .showProgress(true)
+                .setAnimation(Splashy.Animation.SLIDE_IN_LEFT_BOTTOM, 1000)
+                .show();
+    }
+
+    void checkLogin(){
+        new Splashy(this).setInfiniteDuration(true).show();   // For JAVA : new Splashy(this)
+
+        // Some mock example response operation
+//        Response.onResponse(object  : Response.onResponse{
+//            override fun onResponse(response){
+//                Splashy.hide()				// Hide after operation
+//            }
+//
+//        }
+
+        // Listener for completion of splash screen
+//        Splashy.onComplete(object : Splashy.OnComplete {
+//            override fun onComplete() {
+//                Toast.makeText(this@MainActivity, "Logged In", Toast.LENGTH_SHORT).show()
+//            }
+//
+//        })
     }
 }
