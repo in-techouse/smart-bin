46db43c08e70979d51922d122052cb5d3a4f57c0
Tahira Baig <tahirabaig786@hotmail.com>
2019-11-24 11:44:04 +0500

    working On ForgetPasword

--- app/src/main/java/lcwu/fyp/smartbin/ForgetPaswordActivity.java
+++ app/src/main/java/lcwu/fyp/smartbin/ForgetPaswordActivity.java
@@ -1,5 +1,6 @@
 package lcwu.fyp.smartbin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,12 +8,20 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Switch;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgetPaswordActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnForSubmit;
+    String strForEmail;
+    ProgressBar ForgetProgress;
 
     EditText edtForEmail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,7 @@ public class ForgetPaswordActivity extends AppCompatActivity implements View.OnC
 
         btnForSubmit = findViewById(R.id.btnForSubmit);
         edtForEmail = findViewById(R.id.edtForEmail);
+        ForgetProgress = findViewById(R.id.ForgetProgress);
 
         btnForSubmit.setOnClickListener(this);
     }
@@ -28,21 +38,53 @@ public class ForgetPaswordActivity extends AppCompatActivity implements View.OnC
     @Override
     public void onClick(View v) {
         int id = v.getId();
-            switch (id){
-                case R.id.btnForSubmit: {
+        switch (id) {
+            case R.id.btnForSubmit: {
+                strForEmail = edtForEmail.getText().toString();
 
-                    String strForEmail = edtForEmail.getText().toString();
+                boolean flag = isVisibal();
+                if (flag) {
+                    //Firebase
+                    ForgetProgress.setVisibility(View.VISIBLE);
+                    btnForSubmit.setVisibility(View.GONE);
+                    FirebaseAuth auth = FirebaseAuth.getInstance();
+                    auth.sendPasswordResetEmail(strForEmail)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    ForgetProgress.setVisibility(View.GONE);
+                                    btnForSubmit.setVisibility(View.VISIBLE);
 
-                    if (strForEmail.length() < 6 || !Patterns.EMAIL_ADDRESS.matcher(strForEmail).matches()) {
-                        edtForEmail.setError("Enter a Valid email");
-                    } else {
-                        edtForEmail.setError(null);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            ForgetProgress.setVisibility(View.GONE);
+                            btnForSubmit.setVisibility(View.VISIBLE);
+                        }
+                    });
+
+
                     }
+                break;
 
                 }
             }
 
 
+        }
+
+
+    private boolean isVisibal(){
+        boolean flag = true;
+        if (strForEmail.length() < 6 || !Patterns.EMAIL_ADDRESS.matcher(strForEmail).matches()) {
+            edtForEmail.setError("Enter a Valid email");
+            flag = false;
+        }
+        else {
+            edtForEmail.setError(null);
+        }
+            return flag;
 
     }
 }
