6ad0185937a8c84897a3076fa7df6e0bfa84f458
Tahira Baig <tahirabaig786@hotmail.com>
2019-12-28 13:15:44 +0500

    working On Navigation DashBoard

--- app/src/main/java/lcwu/fyp/smartbin/activities/DashBoard.java
+++ app/src/main/java/lcwu/fyp/smartbin/activities/DashBoard.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.provider.ContactsContract;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,18 +25,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.Switch;
+import android.widget.TextView;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import lcwu.fyp.smartbin.R;
 import lcwu.fyp.smartbin.director.Helpers;
 import lcwu.fyp.smartbin.director.Session;
 import lcwu.fyp.smartbin.model.User;
 
-public class DashBoard extends AppCompatActivity {
+public class DashBoard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private AppBarConfiguration mAppBarConfiguration;
     private Session session;
     private Helpers helpers;
     private User user;
+    private CircleImageView Profile_image;
+    private TextView Profile_name;
+    private TextView Profile_email;
+    private DrawerLayout drawer;
 
 
     @Override
@@ -40,31 +51,49 @@ public class DashBoard extends AppCompatActivity {
         setContentView(R.layout.activity_dash_board);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+
         session = new Session(DashBoard.this);
         helpers = new Helpers();
         user = session.getUser();
 
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        View hadder = navigationView.getHeaderView(0);
+        Profile_image = hadder.findViewById(R.id.profile_image);
+        Profile_name = hadder.findViewById(R.id.profile_name);
+        Profile_email = hadder.findViewById(R.id.profile_email);
 
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        Profile_name.setText(user.getFirstName() +" " + user.getLastName());
+        Profile_email.setText(user.getE_mail());
     }
 
 
+
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        switch (id) {
+            case R.id.nav_home: {
+                break;
+            }
+            case R.id.nav_notification: {
+                break;
+            }
+            case R.id.nav_onlinebooking: {
+                break;
+
+            }
+            case R.id.nav_logout:{
+                break;
+            }
+        }
+
+
+        drawer.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
