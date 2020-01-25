c211031c6af1e05abd10b5669285c248640497cc
Tahira Baig <tahirabaig786@hotmail.com>
2020-01-25 13:11:47 +0500

    working On notification and booking activity

--- app/src/main/java/lcwu/fyp/smartbin/activities/BookingActivity.java
+++ app/src/main/java/lcwu/fyp/smartbin/activities/BookingActivity.java
@@ -1,16 +1,27 @@
 package lcwu.fyp.smartbin.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import lcwu.fyp.smartbin.R;
 
 public class BookingActivity extends AppCompatActivity {
 
+    private LinearLayout loading;
+    private TextView noBooking;
+    private RecyclerView bookings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+
+        loading= findViewById(R.id.loading);
+        noBooking = findViewById(R.id.noBooking);
+        bookings = findViewById(R.id.bookings);
     }
 }
--- app/src/main/java/lcwu/fyp/smartbin/activities/NotificationActivity.java
+++ app/src/main/java/lcwu/fyp/smartbin/activities/NotificationActivity.java
@@ -1,16 +1,28 @@
 package lcwu.fyp.smartbin.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import lcwu.fyp.smartbin.R;
 
 public class NotificationActivity extends AppCompatActivity {
 
+    private LinearLayout loading;
+    private TextView noNotification;
+    private RecyclerView notifications;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+
+        loading = findViewById(R.id.loading);
+        noNotification= findViewById(R.id.noNotifications);
+        notifications = findViewById(R.id.notifications);
     }
 }
