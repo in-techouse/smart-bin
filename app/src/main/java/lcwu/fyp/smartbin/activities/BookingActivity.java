df36a653ea430071611cb7ed13e0388865f37c73
Tahira Baig <tahirabaig786@hotmail.com>
2020-01-26 13:01:02 +0500

    working On notification and booking java activity

--- app/src/main/java/lcwu/fyp/smartbin/activities/BookingActivity.java
+++ app/src/main/java/lcwu/fyp/smartbin/activities/BookingActivity.java
@@ -1,27 +1,78 @@
 package lcwu.fyp.smartbin.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import lcwu.fyp.smartbin.R;
+import lcwu.fyp.smartbin.director.Helpers;
+import lcwu.fyp.smartbin.director.Session;
+import lcwu.fyp.smartbin.model.Booking;
+import lcwu.fyp.smartbin.model.User;
 
 public class BookingActivity extends AppCompatActivity {
 
     private LinearLayout loading;
     private TextView noBooking;
     private RecyclerView bookings;
+    private Session session;
+    private User user;
+    private Helpers helpers;
+    private List<Booking> data;
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Bookings");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
 
-        loading= findViewById(R.id.loading);
+        loading = findViewById(R.id.loading);
         noBooking = findViewById(R.id.noBooking);
         bookings = findViewById(R.id.bookings);
+        session = new Session(BookingActivity.this);
+        user = session.getUser();
+        helpers = new Helpers();
+        data = new ArrayList<>();
+        loadBooking();
+
+    }
+    private void loadBooking(){
+        if(!helpers.isConnected(BookingActivity.this)){
+            helpers.showError(BookingActivity.this,"Internet Error","No internet connection check your connection try again");
+            return;
+
+        }
+        loading.setVisibility(View.VISIBLE);
+        noBooking.setVisibility(View.GONE);
+        bookings.setVisibility(View.GONE);
+        reference.orderByChild("userId").equalTo(user.getId()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                loading.setVisibility(View.GONE);
+                noBooking.setVisibility(View.VISIBLE);
+                bookings.setVisibility(View.GONE);
+
+            }
+        });
+
     }
 }
--- app/src/main/java/lcwu/fyp/smartbin/activities/NotificationActivity.java
+++ app/src/main/java/lcwu/fyp/smartbin/activities/NotificationActivity.java
@@ -1,19 +1,41 @@
 package lcwu.fyp.smartbin.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import lcwu.fyp.smartbin.R;
+import lcwu.fyp.smartbin.director.Helpers;
+import lcwu.fyp.smartbin.director.Session;
+import lcwu.fyp.smartbin.model.Booking;
+import lcwu.fyp.smartbin.model.Notification;
+import lcwu.fyp.smartbin.model.User;
 
 public class NotificationActivity extends AppCompatActivity {
 
     private LinearLayout loading;
     private TextView noNotification;
     private RecyclerView notifications;
+    private Session session;
+    private User user;
+    private Helpers helpers;
+    private List<Notification> data;
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Notifications");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +44,38 @@ public class NotificationActivity extends AppCompatActivity {
 
 
         loading = findViewById(R.id.loading);
-        noNotification= findViewById(R.id.noNotifications);
+        noNotification = findViewById(R.id.noNotifications);
         notifications = findViewById(R.id.notifications);
+        session = new Session(NotificationActivity.this);
+        user = session.getUser();
+        helpers = new Helpers();
+        data = new ArrayList<>();
+        loadNotification();
+    }
+
+    private void loadNotification() {
+        if (!helpers.isConnected(NotificationActivity.this)) {
+            helpers.showError(NotificationActivity.this, "Internet Error", "No internet connection check your connection try again");
+            return;
+        }
+
+        loading.setVisibility(View.VISIBLE);
+        noNotification.setVisibility(View.GONE);
+        notifications.setVisibility(View.GONE);
+        reference.orderByChild("userId").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                loading.setVisibility(View.GONE);
+                noNotification.setVisibility(View.VISIBLE);
+                notifications.setVisibility(View.GONE);
+
+            }
+        });
+
     }
 }
