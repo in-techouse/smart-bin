89d02d2d672e6abf7706d610727299e00880ad4d
zeeshan ashraf <zeeshanashraf9t@gmail.com>
2020-02-19 12:31:55 +0500

    Phone Editing Enabled

--- app/src/main/java/lcwu/fyp/smartbin/activities/UserProfileActivity.java
+++ app/src/main/java/lcwu/fyp/smartbin/activities/UserProfileActivity.java
@@ -69,14 +69,6 @@ public class UserProfileActivity extends AppCompatActivity implements View.OnCli
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
         profileFirstName = findViewById(R.id.profileFirstName);
         profileLastName = findViewById(R.id.profileLastName);
